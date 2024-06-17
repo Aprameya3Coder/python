14d67864bc217045ea98efce2b8a190507b85538
Aprameya3Coder <aprameyanathcoder@gmail.com>
2024-06-17 22:02:37 +0530

    Do List kaj

---
+++ python_list/list3.py
@@ -0,0 +1,26 @@
+# # Using `pop` (without index):
+# # - Remove the last item from the `colors` list. Input your command in the terminal and print the
+# # # `colors` list to see the result.
+
+colour = ['Red','Green','Magenta','Yellow','Cyan','Orange']
+print(colour) 
+
+# removed_colour = colour.pop()
+# print(removed_colour)
+# print(colour)
+
+
+# 6. Using `pop` (with index):
+# Remove the first item (index 0) from the `colors` list. Input your command in the terminal
+# and print the `colors` list to see the result
+# remove_third_colour = colour.pop(2)
+# print(colour)
+# print(remove_third_colour)
+
+
+
+# 7. Using `remove`:
+# Remove the color "green" from the `colors` list. Input your command in the terminal and
+# print the `colors` list to see the result
+removed_green_colour = colour.remove("Green")
+print(colour)
