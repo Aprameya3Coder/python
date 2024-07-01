d0200f9014c06720a23f9dc223064ceaea2f128b
Aprameya3Coder <aprameyanathcoder@gmail.com>
2024-07-01 18:34:15 +0530

    Dear Rohan Brother,
    Today I've done a lot of work  . I request you to check  the homework done by me .
    
    Thanking You
    
    Your loving student  ,  Aprameya Nath

---
+++ Homework_AdvPython/dictionary.py
@@ -0,0 +1,16 @@
+Natural_Elements = {
+    "water":"blue",
+    "soil":"brown",
+    "plants":"green"
+}
+#Accessing a specific element using a key:
+# plants_colour = Natural_Elements["plants"]
+# print(plants_colour) #this will print "green"
+
+#Keys:
+#print(Natural_Elements.keys())
+#Print all the keys : "water","soil","plants"
+
+#Values:
+#print(Natural_Elements.values())
+#Print all the values : "water","soil","plants"
---
+++ Homework_AdvPython/tuple.py
@@ -0,0 +1,27 @@
+vehicles = ("Truck","Bus","Car","Bike")
+
+# # # Accesing the first element 
+# first_vehicle = vehicles[0]
+# print(first_vehicle) 
+
+# # Accessing the second element
+# second_vehicle = vehicle[1]
+# print(second_vehicle) #This will print "Bus"
+
+# #Length:
+# vehicle("Truck","Bus","Car","Bike")
+# aprint(len(vehicle)) 
+
+# #Count:
+# #numbers("2","8","1","3")
+# #print(numbers.count) 
+
+#Index:
+#Software_Company("Google","Microsoft","Adobe")
+#print(Software_Company.index("Microsoft")) 
+
+#tuple1 = (2,3,4)
+#tuple2 = (5,6)
+#result = tuple1 + tuple2
+##print(result) 
+
